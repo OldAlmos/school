1b4df68674e844cdf83c8e04232b40013722af90
OldAlmos <mopy27512@gmail.com>
2020-01-30 22:25:13 +0300

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -11,11 +11,11 @@
 
 
 num_list=input('Input unsort sequence: ').split()
+num_list = [int(item) for item in num_list]
 len_list=len(num_list)
 while (len_list>1):
 	for i in range(0, len_list-1):
-		if int(num_list[i])>int(num_list[i+1]):
+		if num_list[i]>num_list[i+1]:
 			num_list[i], num_list[i+1]=num_list[i+1], num_list[i]
-		i+=1
 	len_list-=1
 print(num_list)
